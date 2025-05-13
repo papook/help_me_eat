1d8938f4556fe89d1c000f43c74637b6a142ea7f
papook <50879652+papook@users.noreply.github.com>
2025-05-13 20:17:24 +0200

    feat: :sparkles: implement MealCategoryTile widget and MealCategory enum
    1. Implemented the MealCategoryTile widget, which displays information about different meal categories (breakfast, lunch, dinner, snacks) and also allows the user to add a new meal to these categories. 2. Created an Enum to easily construct MealCategoryTile automatically and in a modular way based on the category.

--- lib/widgets/meals_list.dart
+++ lib/widgets/meals_list.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class MealsList extends StatelessWidget {
   MealsList({super.key});
   // TODO: Create a list of meal categories
-  final _categories = [];
+  final _categories = [
+    MealCategoryTile(mealCategory: MealCategory.breakfast),
+    MealCategoryTile(mealCategory: MealCategory.lunch),
+    MealCategoryTile(mealCategory: MealCategory.dinner),
+    MealCategoryTile(mealCategory: MealCategory.snacks),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,4 +34,61 @@ class MealsList extends StatelessWidget {
       ),
     );
   }
+}
+
+class MealCategoryTile extends StatelessWidget {
+  final MealCategory mealCategory;
+  // TODO: Replace both values with actual data from the provider
+  final double intakeCals = 200;
+  final double goalCals = 500;
+
+  const MealCategoryTile({super.key, required this.mealCategory});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        margin: const EdgeInsets.only(right: 5),
+        child: mealCategory.iconWidget,
+      ),
+      title: Text(
+        mealCategory.mealName,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text(
+        '${intakeCals.round()} / ${goalCals.round()} kcal',
+        style: TextStyle(color: Theme.of(context).colorScheme.primary),
+      ),
+      trailing: IconButton(
+        // TODO: Implement the method to add a meal
+        onPressed: () {},
+        icon: const Icon(Icons.add_circle_outline_outlined),
+      ),
+    );
+  }
+}
+
+enum MealCategory {
+  breakfast(
+    mealName: 'Breakfast',
+    iconWidget: Icon(Icons.breakfast_dining, size: _iconSize),
+  ),
+  lunch(
+    mealName: 'Lunch',
+    iconWidget: Icon(Icons.lunch_dining, size: _iconSize),
+  ),
+  dinner(
+    mealName: 'Dinner',
+    iconWidget: Icon(Icons.dinner_dining, size: _iconSize),
+  ),
+  snacks(
+    mealName: 'Snacks',
+    iconWidget: Icon(Icons.local_drink, size: _iconSize),
+  );
+
+  static const double _iconSize = 30.0;
+  final String mealName;
+  final Icon iconWidget;
+
+  const MealCategory({required this.mealName, required this.iconWidget});
 }
